955d0f3820601b219a74cc9e252c6052d44ddd6c
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-15 17:38:31 +0300

    feat: Добавить образцы новостей

---
+++ resources/sourceExample.js
@@ -0,0 +1,64 @@
+const sourceExample = [
+  {
+    title: 'Чиновникам разрешат сбивать пешеходов при чрезвычайных обстоятельствах',
+    author: 'Ефим Аноним',
+    date: '09:59 15.03.2021',
+    link: 'https://panorama.pub/news/chinovnikam-razreshat-sbivat-peshexodov-pri-chrezvychajnyx-obstoyatelstvax'
+  },
+  {
+    title: 'Для борьбы с алкоголизмом Минздрав предложил уменьшить размер рюмки',
+    author: 'Виталий Манн',
+    date: '09:16 15.03.2021',
+    link: 'https://panorama.pub/news/dlya-borby-s-alkogolizmom-minzdrav-predlozhil-umenshit-razmer-ryumki'
+  },
+  {
+    title: 'Камеру на Рублёвском шоссе демонтировали из-за клеветы на чиновников',
+    author: 'Эрвин Кляйн',
+    date: '09:59 15.03.2021',
+    link: 'https://panorama.pub/news/kameru-na-rublyovskom-shosse-demontirovali-iz-za-klevety-na-chinovnikov'
+  },
+  {
+    title: 'Роботы Boston Dynamics создали профсоюз',
+    author: 'Потап Романов',
+    date: '19:23 06.03.2021',
+    link: 'https://panorama.pub/news/roboty-boston-dynamics-sozdali-profsoyuz'
+  },
+  {
+    title: 'Байден подписал указ о скачкообразном повышении цен в России',
+    author: 'Ефим Аноним',
+    date: '08:40 10.03.2021',
+    link: 'https://panorama.pub/news/bajden-podpisal-ukaz-o-skachkoobraznom-uvelichenii-czen-na-produkty-v-rossii'
+  },
+  {
+    title: 'Дмитрий Гордон станет рекламным лицом Head&Shoulders',
+    author: 'Константин Ковидный',
+    date: '19:12 14.03.2021',
+    link: 'https://panorama.pub/news/dmitrij-gordon-stanet-reklamnym-liczom-shampunej-headshoulders'
+  },
+  {
+    title: 'Росавиация хочет регламентировать полёты пчёл',
+    author: 'Виталий Манн',
+    date: '15:35 14.03.2021',
+    link: 'https://panorama.pub/news/rosaviacziya-xochet-reglamentirovat-polyoty-pchyol'
+  },
+  {
+    title: 'Marvel выкупила права на Чебурашку и создаст на его основе супергероя',
+    author: 'Эрвин Кляйн',
+    date: '18:43 13.03.2021',
+    link: 'https://panorama.pub/news/marvel-vykupila-prava-na-cheburashku-i-sozdast-na-ego-osnove-supergeroya'
+  },
+  {
+    title: 'Госдума приняла закон о запрете дарения носков на 23 февраля',
+    author: 'Виталий Манн',
+    date: '08:50 14.03.2021',
+    link: 'https://panorama.pub/news/gosduma-prinyala-zakon-o-zaprete-dareniya-noskov-na-23-fevralya'
+  },
+  {
+    title: 'Учёные пришли к выводу, что Санта Клаус был моноголом',
+    author: 'Анастасия Петрик',
+    date: '22:09 30.12.2020',
+    link: 'https://panorama.pub/news/p53744'
+  }
+];
+
+export { sourceExample };
