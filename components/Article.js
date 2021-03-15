0dbaf935904a105514410a45542345d44d2150a2
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-15 16:39:47 +0300

    fix: Исправить Article

--- components/Article.js
+++ components/Article.js
@@ -17,28 +17,28 @@ export default class Article {
     this._titleElement = document.createElement('h2');
     this._titleElement.classList.add('article__title');
     this._titleElement.textContent = this._title;
-    this._element.prepend(this._titleElement);
+    this._element.append(this._titleElement);
 
     this._button = document.createElement('button');
     this._button.classList.add('article__status');
-    this._element.prepend(this._button);
+    this._element.append(this._button);
 
     this._authorElement = document.createElement('p');
     this._authorElement.classList.add('article__author');
     this._authorElement.textContent = this._author;
-    this._element.prepend(this._authorElement);
+    this._element.append(this._authorElement);
 
     this._dateElement = document.createElement('p');
     this._dateElement.classList.add('article__date');
     this._dateElement.textContent = this._date;
-    this._element.prepend(this._dateElement);
+    this._element.append(this._dateElement);
 
     this._linkElement = document.createElement('a');
     this._linkElement.classList.add('article__link');
     this._linkElement.textContent = 'Подробнее';
     this._linkElement.setAttribute('href', this._link);
     this._linkElement.setAttribute('target', '_blank');
-    this._element.prepend(this._linkElement);
+    this._element.append(this._linkElement);
 
     return this._element;
   }
@@ -46,6 +46,6 @@ export default class Article {
   add () {
     const article = this._create();
 
-    this._container.prepend(article);
+    this._container.append(article);
   }
 }
