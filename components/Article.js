f0c0199696f6b72a4ab443082049deeb85a31674
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-15 16:25:34 +0300

    feat: Прописать поля класса Article

--- components/Article.js
+++ components/Article.js
@@ -1,5 +1,12 @@
 export default class Article {
-  constructor () {
-    
+  constructor (container, data) {
+    this._container = container;
+
+    this._title = data.title;
+    this._author = data.author;
+    this._date = data.date;
+    this._link = data.link;
+
+    this._isRead = false;
   }
 }
