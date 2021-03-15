36f2130a3207bc4d3943aa028610c3e7e9272ca0
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-15 16:32:40 +0300

    feat: Прописать метод _create в Article

--- components/Article.js
+++ components/Article.js
@@ -9,4 +9,37 @@ export default class Article {
 
     this._isRead = false;
   }
+
+  _create () {
+    this._element = document.createElement('article');
+    this._element.classList.add('widget__article', 'article');
+
+    this._titleElement = document.createElement('h2');
+    this._titleElement.classList.add('article__title');
+    this._titleElement.textContent = this._title;
+    this._element.prepend(this._titleElement);
+
+    this._button = document.createElement('button');
+    this._button.classList.add('article__status');
+    this._element.prepend(this._button);
+
+    this._authorElement = document.createElement('p');
+    this._authorElement.classList.add('article__author');
+    this._authorElement.textContent = this._author;
+    this._element.prepend(this._authorElement);
+
+    this._dateElement = document.createElement('p');
+    this._dateElement.classList.add('article__date');
+    this._dateElement.textContent = this._date;
+    this._element.prepend(this._dateElement);
+
+    this._linkElement = document.createElement('a');
+    this._linkElement.classList.add('article__link');
+    this._linkElement.textContent = 'Подробнее';
+    this._linkElement.setAttribute('href', this._link);
+    this._linkElement.setAttribute('target', '_blank');
+    this._element.prepend(this._linkElement);
+
+    return this._element;
+  }
 }
