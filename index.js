cef300b9e7a3afc8072b1973bc635c1fc794513a
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-15 17:41:58 +0300

    feat: Добавить новости в виджет

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import Widget from './components/Widget.js';
 import Article from './components/Article.js';
+import { sourceExample } from './resources/sourceExample.js';
 
 const targetElement = document.querySelector('aside');
-
 const widget = new Widget(targetElement);
-
 widget.add();
+
+const widgetContainer = document.querySelector('.widget__container');
+
+sourceExample.forEach(item => {
+  const article = new Article(widgetContainer, item);
+  article.add();
+});
