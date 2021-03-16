0f492a683202d8a691e944e578261bd9e89fe160
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-16 11:52:59 +0300

    feat: Прописать разные источники новостей

--- index.js
+++ index.js
@@ -1,19 +1,61 @@
 import Widget from './components/Widget.js';
 import Article from './components/Article.js';
-import { sourceExample } from './resources/sourceExample.js';
+import { sourceExample, sourceExample_2 } from './resources/sourceExample.js';
+import { api } from './utils/Api.js';
+
+// * Добавить виджет на страницу
 
 const targetElement = document.querySelector('aside');
 
 const widget = new Widget(targetElement);
 widget.addWidget();
 
-sourceExample.forEach(item => {
-  const article = new Article(
-    item,
-    () => widget.refreshUnreadCounter()
-  );
-  const articleElement = article.createElement();
-  widget.insertArticle(article, articleElement);
-});
+// * Получение новостей
 
-widget.refreshUnreadCounter();
+function loadLocalNews (source) { // Получение новостей из локального источника
+  source.forEach(item => {
+    const article = new Article(
+      item,
+      () => widget.refreshUnreadCounter()
+    );
+    const articleElement = article.createElement();
+    widget.insertArticle(article, articleElement);
+  });
+
+  widget.refreshUnreadCounter();
+}
+
+function loadRemoteNews () { // Получение новостей из удалённого источника
+  api.getNews()
+    .then(res => {
+      const articles = res.articles;
+
+      const processedArticles = articles.map(article => {
+        const processedArticle = {};
+
+        processedArticle.title = article.title;
+        processedArticle.author = article.source.name;
+        processedArticle.date = article.publishedAt;
+        processedArticle.link = article.url;
+
+        return processedArticle;
+      });
+
+      return processedArticles;
+    })
+    .then(processedArticles => {
+      processedArticles.forEach(item => {
+        const article = new Article(
+          item,
+          () => widget.refreshUnreadCounter()
+        );
+        const articleElement = article.createElement();
+        widget.insertArticle(article, articleElement);
+      })
+
+      widget.refreshUnreadCounter();
+    })
+    .catch(err => console.log(err));
+}
+
+loadLocalNews(sourceExample);
