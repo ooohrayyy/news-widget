9e77b4c94fa2e28bc837a8d2df9e8cfe64ca7aad
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-16 10:55:22 +0300

    feat: Добавить проверку ответа в Api

--- utils/Api.js
+++ utils/Api.js
@@ -3,4 +3,11 @@ class Api {
     this._baseUrl = options.baseUrl;
     this._key = options.key;
   }
+
+  _checkResponseData (res) {
+    if (!res.ok) {
+      return Promise.reject(`Ошибка: ${res.status}`);
+    }
+    return res.json();
+  }
 }
