1f5a7f5c08b74cffaea472fbf21317d60be8c6d0
Boris Nikitashov <ooohrayyy@gmail.com>
2021-03-16 12:22:23 +0300

    feat: Добавить кнопки управления виджетом

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ const widget = new Widget(targetElement);
 
 widget.add();
 
+// * Добавить управление виджетом на страницу
+
+const parent = document.querySelector('body');
+const sibling = document.querySelector('aside');
+
+const buttonOne = document.createElement('button');
+buttonOne.textContent = 'Локальный источник';
+buttonOne.addEventListener('click', () => {
+  loadLocalNews(sourceExample);
+});
+
+const buttonTwo = document.createElement('button');
+buttonTwo.textContent = 'Локальный источник [2]';
+buttonTwo.addEventListener('click', () => {
+  loadLocalNews(sourceExample_2);
+});
+
+const buttonThree = document.createElement('button');
+buttonThree.textContent = 'Удалённый источник',
+buttonThree.addEventListener('click', () => {
+  loadRemoteNews();
+});
+
+parent.insertBefore(buttonOne, sibling);
+parent.insertBefore(buttonTwo, sibling);
+parent.insertBefore(buttonThree, sibling);
+
 // * Получение новостей
 
 function loadLocalNews (source) { // Получение новостей из локального источника
